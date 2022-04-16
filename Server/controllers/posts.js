f1c846fcb67d2249ff4ee200352bace669dd9e62
Aman Verma <av.amanverma.11@gmail.com>
2022-04-16 15:18:09 +0530

    Like a post

--- Server/controllers/posts.js
+++ Server/controllers/posts.js
@@ -36,10 +36,15 @@ export const deletePost = async (req, res) => {
     if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No posts with that id !');
     
     await PostMessage.findByIdAndRemove(id);
-    console.log('DELETED!');
     res.json({message:':Post Deleted Successfully'});
 }
 
 export const likePost = async (req,res) => {
-    res.json.send('Like');
+    const {id} = req.params;
+    
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No posts with that id !');
+    const post = await PostMessage.findById(id);
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, {likeCount : post.likeCount + 1}, {new:true});
+    console.log('Liked');
+    res.json(updatedPost);
 }
