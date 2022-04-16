05879bcc1873f24aeeb0b459a866543138af5b48
Aman Verma <av.amanverma.11@gmail.com>
2022-04-16 16:05:09 +0530

    Environment variable

--- Server/index.js
+++ Server/index.js
@@ -3,19 +3,19 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import postRoutes from './routes/posts.js';
-
+import dotenv from 'dotenv';
 
 const app = express();
-
+dotenv.config();
 app.use(bodyParser.json({limit:"30mb", extended:true}));
 app.use(bodyParser.urlencoded({limit:"30mb", extended:true}));
 app.use(cors());
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://amanverma:amanverma11@cluster0.xk7gn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+// const CONNECTION_URL = 'mongodb+srv://amanverma:amanverma11@cluster0.xk7gn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
-mongoose.connect(CONNECTION_URL, {useNewUrlParser:true, useUnifiedTopology:true})
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser:true, useUnifiedTopology:true})
 .then(()=> app.listen(PORT, ()=> console.log(`Server running on port :${PORT} `)))
 .catch((error)=> console.log(error.message));
 
-// mongoose.set('useFindAndModify', false);
+//mongoose.set('useFindAndModify', false);
